9485fbd3366a70794f9d3e1b148f451a1ce6427e
firkeuf <bsd4fir@gmail.com>
2014-07-22 16:55:43 +0300

    Update urls.py
    url regexp

--- asterisk_monitor/ast_rec/urls.py
+++ asterisk_monitor/ast_rec/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
                        url(r'^$', views.call_list_view, name='call_list_view'),
                        url(r'^sc$', views.synchronous_calls, name='synchronous_calls'),
                        url(r'^bhc$', views.by_hour_calls, name='by_hour_calls'),
-                       url(r'^records/(?P<uniqueid>\w+)$', views.records_xsendfile,
+                       url(r'^records/(?P<uniqueid>\w.*)$', views.records_xsendfile,
                            {'document_root': settings.RECORDS_ROOT, }, name='records_xsendfile'),
 
                        # url(r'^vote/$', views.vote, name='vote'),
